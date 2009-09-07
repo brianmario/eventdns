9624804d71c87b8950d56062103aa19f85e0a057
Joël Franusic <joel@franusic.com>
2009-09-06 21:05:59 -0700

    Running a query at startup to test the backend.

--- lib/eventdns.rb
+++ lib/eventdns.rb
@@ -4,6 +4,28 @@ class EventDns < EventMachine::Connection
   def initialize
     @backend = UseBackend.new(CONFIG[:driver])
     $logger.debug "Handling query via the #{CONFIG[:driver]} Backend."
+
+    return unless CONFIG[:driver] == 'http'
+
+    # Push a test query through our driver to make sure we have connectivity.
+    #FIXME: This needs to get cleaned up, made smaller, or something.
+    test = Dnsruby::Message.new
+    test.add_question('pi.http.viadns.org', Dnsruby::Types.A, Dnsruby::Classes.IN)
+
+    test.question.each do |q|
+      $logger.debug "Startup test: Requesting an #{q.qtype} record for #{q.qname}"
+      @backend.handle(q,test)
+    end
+
+    error = false
+    error = true unless test.answer[0].name.to_s == 'pi.http.viadns.org'
+    error = true unless test.answer[0].address.to_s == '3.14.159.26'
+    if error
+      throw LoadError "Unable to run test query!"
+    else
+      $logger.debug "Test successful!"
+    end
+      
   end
   
   def new_connection
